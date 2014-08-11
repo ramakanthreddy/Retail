3267dcc25d8ba3e6d9816d5e98a5d679c66952a2
nirakarpanda <nirakarpanda@hotmail.com>
2014-08-11 11:38:11 +0530

    bvbbv

--- src/main/java/com/javahome/web/controllers/StockController.java
+++ src/main/java/com/javahome/web/controllers/StockController.java
@@ -1,17 +1,35 @@
 package com.javahome.web.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.javahome.app.service.IStockAddService;
+import com.javahome.app.service.StockAddService;
+import com.javahome.web.vo.StockVO;
 
 
 @Controller
 public class StockController {
 
-	@RequestMapping(value="/addStock.htm",method=RequestMethod.GET)
+	@Autowired
+	private StockAddService stockAddService;
+	@RequestMapping(value="/addStock",method=RequestMethod.GET)
 public String addStock(Model model){
+		StockVO stockvo = new StockVO();
+		model.addAttribute("addstock",stockvo);
+		
 	
-	return null;
+	return "addStock";
 }
+	@RequestMapping(value = "/addStock", method = RequestMethod.POST)
+	public String addStock(@ModelAttribute("stockvo") StockVO stockvo) {
+		
+		
+	stockAddService.StockAdd(stockvo);
+		return "addStock";
+	}
 }
