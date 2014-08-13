fb69df2f58ef3f2e709c431c6f2fbe3771ece783
ramakanth.konday <ramakanth.konday@gmail.com>
2014-08-13 09:01:00 +0530

    Removed code which is related to depricted one

--- src/main/java/com/javahome/web/controllers/StockController.java
+++ src/main/java/com/javahome/web/controllers/StockController.java
@@ -1,14 +1,11 @@
 package com.javahome.web.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.javahome.app.service.IStockAddService;
-import com.javahome.app.service.StockAddService;
 import com.javahome.web.vo.StockVO;
 
 
